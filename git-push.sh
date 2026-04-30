d35d839fbf22c128aabbf944173c1b90f64a10cb
Mohau TheCool Ramakhula <mramakhula@fix.co.ls>
2026-04-30 09:12:24 +0200

    test: verify push works

--- git-push.sh
+++ git-push.sh
@@ -3,5 +3,5 @@ MESSAGE=${1:-"chore: update"}
 cd ~/ithemba
 git add -A
 git commit -m "$MESSAGE"
-GIT_TERMINAL_PROMPT=0 GIT_ASKPASS="" git push
+GIT_ASKPASS="" SSH_ASKPASS="" git push
 echo "✅ Pushed: $MESSAGE"
